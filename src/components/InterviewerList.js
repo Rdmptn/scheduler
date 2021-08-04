cf3ebe2fb30c55abd9cffcb38b9785b3808ab361
Cameron <cameron_uniac@hotmail.com>
2021-08-04 16:46:44 -0400

    Create InterviewerList and Appointment components

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classnames from "classnames";
+
+import "components/InterviewerList.scss";
+
+import InterviewerListItem from "components/InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const interviewerData = props.interviewers;
+  
+  const displayInterviewers = interviewerData.map((interviewerData, index) => {
+    return <InterviewerListItem key={interviewerData.id} name={interviewerData.name} 
+            avatar={interviewerData.avatar} selected={interviewerData.id === props.interviewer} 
+            setInterviewer={(event) => props.setInterviewer(interviewerData.id)}  />          
+  })
+  
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{displayInterviewers}</ul>
+    </section>
+  );
+}
