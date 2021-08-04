73871073510e5b19c3b70fb8750e3082efe778b1
Cameron <cameron_uniac@hotmail.com>
2021-08-04 02:47:08 -0400

    Add stories and test display for interviwer list items

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classnames from "classnames";
+
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+  
+  const displayName = function (selected, name) {
+    if (selected) {
+      return name;
+    }
+  }
+
+  return (
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {displayName(props.selected, props.name)}
+    </li>
+    );
+
+}
