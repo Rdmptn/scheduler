1330475d410c4e33385299d7592e49570adf8374
Cameron <cameron_uniac@hotmail.com>
2021-08-16 23:59:28 -0400

    Add comments for custom hooks

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+//Grabs data from the api and sets the state for each day to the given data,
+//and allows for creation, editing, or deletion of appointments
 export default function useApplicationData(initial) {
   
+  //Initial state before api request is made
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
   });
+  
   const setDay = day => setState({ ...state, day });
   
+  //Api requests for 
   useEffect(() => {
     Promise.all([
       axios.get('api/days'),
@@ -21,6 +26,7 @@ export default function useApplicationData(initial) {
       })
   }, []) 
   
+  //Gets the id of the given day for later use
   const getDayId = function(today, days) {
     let dayId;  
     for (let day of days) {
@@ -31,6 +37,7 @@ export default function useApplicationData(initial) {
     return dayId;
   }
   
+  //Checks the amount of null interviews for a given day, and returns that value
   const updateSpots = function (dayId, days, appointments) {
     let nullCount = 0;
     let appointmentsForDay = days[dayId].appointments;
@@ -42,6 +49,7 @@ export default function useApplicationData(initial) {
     return nullCount;
   }
   
+  //Edits an existing interview or creates a new one and adds it the api for the given id
   const bookInterview = function(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -67,6 +75,8 @@ export default function useApplicationData(initial) {
     }); 
   }
   
+  
+  //Deletes an existing interview appointment, changes the inteview state to null, and updates spots
   const cancelInterview = function(id) {
     
     let dayId = getDayId(state.day, state.days);
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,12 @@
 import { useState } from "react"; 
 
+//Exports the "mode" state to the app which controls the display
+//of each interview appointment to its corresponding mode
 export default function useVisualMode(initial) {
   const [mode, setMode] =  useState(initial);
   const history = [initial];
   
+  //Sets the mode to the given value, and stores it in the array
   const transition = function(newMode, replace = false) {
     setMode(newMode);
     if (replace) {
@@ -11,7 +14,8 @@ export default function useVisualMode(initial) {
     }
     history.push(newMode);
   }
-  
+   
+  //Sets the mode to the previous value stored in the history array
   const back = function() {
     if (history.length > 1) {
       history.pop();
